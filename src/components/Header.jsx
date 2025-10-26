4c51d5a09206543b18a675e7c4d400db68dc4847
Martin Iulia Maria <martinmaria411@gmail.com>
2025-10-26 19:10:57 +0200

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,68 +10,20 @@ import { addFakeRestaurantsAndReviews } from "@/src/lib/firebase/firestore.js";
 import { setCookie, deleteCookie } from "cookies-next";
 
 function useUserSession(initialUser) {
-  return;
-}
+  useEffect(() => {
+    return onIdTokenChanged(async (user) => {
+      if (user) {
+        const idToken = await user.getIdToken();
+        await setCookie("__session", idToken);
+      } else {
+        await deleteCookie("__session");
+      }
+      if (initialUser?.uid === user?.uid) {
+        return;
+      }
+      window.location.reload();
+    });
+  }, [initialUser]);
 
-export default function Header({ initialUser }) {
-  const user = useUserSession(initialUser);
-
-  const handleSignOut = (event) => {
-    event.preventDefault();
-    signOut();
-  };
-
-  const handleSignIn = (event) => {
-    event.preventDefault();
-    signInWithGoogle();
-  };
-
-  return (
-    <header>
-      <Link href="/" className="logo">
-        <img src="/friendly-eats.svg" alt="FriendlyEats" />
-        Friendly Eats
-      </Link>
-      {user ? (
-        <>
-          <div className="profile">
-            <p>
-              <img
-                className="profileImage"
-                src={user.photoURL || "/profile.svg"}
-                alt={user.email}
-              />
-              {user.displayName}
-            </p>
-
-            <div className="menu">
-              ...
-              <ul>
-                <li>{user.displayName}</li>
-
-                <li>
-                  <a href="#" onClick={addFakeRestaurantsAndReviews}>
-                    Add sample restaurants
-                  </a>
-                </li>
-
-                <li>
-                  <a href="#" onClick={handleSignOut}>
-                    Sign Out
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="profile">
-          <a href="#" onClick={handleSignIn}>
-            <img src="/profile.svg" alt="A placeholder user image" />
-            Sign In with Google
-          </a>
-        </div>
-      )}
-    </header>
-  );
+  return initialUser;
 }
